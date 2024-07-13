e2d8ab7626c06ed4f4e5e03f46fbc4dbb9240580
yousufSakib <yousufsakib29603@gmail.com>
2024-07-13 09:05:40 +0600

    Front end design about complete

---
+++ taskManager/route.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.router();
+
+router.get("tasks", async (req, res, next) => {});
+
+route.post("tasks", async (req, res, next) => {});
+
+router
+    .route("tasks/:id")
+    .get(async (req, res, next) => {})
+
+    .patch(async (req, res, next) => {})
+
+    .delete(async (req, res, next) => {});
